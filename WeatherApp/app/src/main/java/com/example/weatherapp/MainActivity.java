eb833c59f2665eb60c75a73fb9c9d2c8d69e0069
SergSerg <sergio.ne@inbox.ru>
2020-04-13 13:52:18 +0600

    Commit добавил обработку кнопки Show Weather c открытием экрана ScreeenMainActivity

--- WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
+++ WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -2,15 +2,31 @@ package com.example.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button showWeather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       showWeather = findViewById(R.id.buttonShowWeather);
+
+        showWeather.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ScreenMainActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
 
